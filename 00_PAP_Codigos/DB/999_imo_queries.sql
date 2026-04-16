2d0a80e58d1c86b45d06272d5e3d2a9483063647
Fabio <fabio.vitoriano@icloud.com>
2026-04-16 09:37:59 +0100

    Criação de Queries.

--- 00_PAP_Codigos/DB/999_imo_queries.sql
+++ 00_PAP_Codigos/DB/999_imo_queries.sql
@@ -5,26 +5,6 @@ SELECT * FROM carrinhos;
 
 SELECT * FROM produtos;
 
--- Query para atualizar o custo_total dos carrinhos com base no custo dos produtos incluidos nele
-UPDATE carrinhos
-SET custo_total = (SELECT SUM(p.preco_venda * cp.quantidade)
-FROM carrinho_produtos AS cp
-INNER JOIN produtos AS p ON cp.id_produto = p.id AND cp.id_carrinho = carrinhos.id)
-WHERE carrinhos.id IN (SELECT id_carrinho FROM carrinho_produtos);  
-
-SELECT SUM(p.preco_venda * cp.quantidade)
-FROM carrinho_produtos AS cp
-INNER JOIN produtos AS p ON cp.id_produto = p.id
-INNER JOIN carrinhos AS c ON cp.id_carrinho = c.id
-GROUP BY cp.id_carrinho;
-
--- Query para ver os pordutos mais vendidos (CARRINHOS)
-SELECT p.nome, SUM(cp.quantidade) AS total_vendido
-FROM carrinho_produtos AS cp
-INNER JOIN produtos AS p ON cp.id_produto = p.id
-INNER JOIN pedidos AS pe ON cp.id_carrinho = pe.id_carrinho
-GROUP BY p.nome
-ORDER BY total_vendido DESC;
 
 -- Filtro para calças
 SELECT * FROM produtos
@@ -58,6 +38,22 @@ ORDER BY total_stock DESC;
 
 
 -- 15 Queries
+
+-- Query para contar o número total de utilizadores
+SELECT COUNT(*)
+FROM utilizadores;
+
+-- Query para contar o número total de produtos
+SELECT COUNT(*)
+FROM produtos;
+
+-- Query para ver a receita de cada produto 
+SELECT p.nome, p.tamanho, SUM(p.preco_venda * cp.quantidade)
+FROM carrinho_produtos AS cp
+INNER JOIN produtos AS p ON cp.id_produto = p.id
+INNER JOIN pedidos AS pe ON cp.id_carrinho = pe.id_carrinho
+GROUP BY p.id;
+
 -- Query para ver os pordutos mais vendidos (CARRINHOS)
 SELECT p.nome, SUM(cp.quantidade) AS total_vendido
 FROM carrinho_produtos AS cp
@@ -65,3 +61,20 @@ INNER JOIN produtos AS p ON cp.id_produto = p.id
 INNER JOIN pedidos AS pe ON cp.id_carrinho = pe.id_carrinho
 GROUP BY p.nome
 ORDER BY total_vendido DESC;
+
+-- Query para atualizar o custo_total dos carrinhos com base no custo dos produtos incluidos nele
+UPDATE carrinhos
+SET custo_total = (SELECT SUM(p.preco_venda * cp.quantidade)
+FROM carrinho_produtos AS cp
+INNER JOIN produtos AS p ON cp.id_produto = p.id AND cp.id_carrinho = carrinhos.id)
+WHERE carrinhos.id IN (SELECT id_carrinho FROM carrinho_produtos);  
+
+-- Query para saber a receita de produtos por categoria
+SELECT p.tipo, SUM(p.preco_venda * cp.quantidade) AS receita
+FROM carrinho_produtos AS cp
+INNER JOIN produtos AS p ON cp.id_produto = p.id
+INNER JOIN pedidos AS pe ON cp.id_carrinho = pe.id_carrinho
+GROUP BY p.tipo;
+
+-- Query para contar utilizadores ativos
+SELECT COUNT(*)
