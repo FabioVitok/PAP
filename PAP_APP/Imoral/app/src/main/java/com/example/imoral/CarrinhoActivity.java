34f2401adaa274edc713ae48f5a9a22216f617a2
Fabio Vitoriano <37599@esjaloures.org>
2026-05-22 00:01:18 +0100

    Alterações

---
+++ PAP_APP/Imoral/app/src/main/java/com/example/imoral/CarrinhoActivity.java
@@ -0,0 +1,25 @@
+package com.example.imoral;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.imoral.models.Produto;
+
+import java.util.ArrayList;
+
+public class CarrinhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
