f9e50adcbc5ed5944da9db967ac5967ff6e11775
FabioVitok <37599@esjaloures.org>
2026-05-21 13:42:57 +0100

    Atualizações na app

--- PAP_APP/Imoral/app/src/main/java/com/example/imoral/LoginActivity.java
+++ PAP_APP/Imoral/app/src/main/java/com/example/imoral/LoginActivity.java
@@ -1,17 +1,29 @@
 package com.example.imoral;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String naoTemContaText;
+
+    TextView naoTemContaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,58 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
    });
+        naoTemContaText = getString(R.string.nao_tem_conta_text);
+        naoTemContaTextView = findViewById(R.id.naoTemContaTextView);
+
+        updateTextViewSpannable();
+
+        loginListener();
+
+    }
+
+    public void updateTextViewSpannable() {
+        naoTemContaTextView.setText(createSpannableClick());
+        naoTemContaTextView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+    public SpannableString createSpannableClick(){
+        SpannableString spannableString = createSpannableText();
+
+        spannableString.setSpan(
+                createClickableSpan(),
+                naoTemContaText.indexOf("Regista-te"),
+                26,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
+    }
+    public SpannableString createSpannableText(){
+        //torna a string spannable sendo possivel mudar o style e se é clicavel
+        SpannableString spannableString = new SpannableString(naoTemContaText);
+
+        return spannableString;
+    }
+    public ClickableSpan createClickableSpan(){
+        //define qual parte do texto podera ser clicavel
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                //Toast.makeText(getApplicationContext(), "Clicado", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint textPaint) {
+                super.updateDrawState(textPaint);
+                textPaint.setColor(Color.WHITE); // coloca cor azul
+                textPaint.setUnderlineText(true); // Coloca o sublinhado
+            }
+        };
+
+        return clickableSpan;
+    }
+
+    public void loginListener() {
         Button buttonLogin = findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +95,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-}
-
-
+    }
 }
